0278be32b03f190800b151be485a673978ec27d3
bobbi <bobbi@bobbi>
2024-09-30 10:01:21 +0300

    test

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0f7de2dded9ecd8320044581fa7953ff29e79c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce72f640e13094e6f7af845b11406fd1ca5728b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
