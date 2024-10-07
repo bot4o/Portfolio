61d5b3e8e74b8d30656553685e90a06d028ca72e
bobbi <bobbi@bobbi>
2024-10-07 07:24:46 +0300

    added links to each button nav

--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0278be32b03f190800b151be485a673978ec27d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786c6ba812b5fc4477badd276d28300a622c821d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
